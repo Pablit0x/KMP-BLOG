aab181d02985af1ac21038f24aea868ffe2f0389
noner <pawel495@interia.pl>
2023-09-15 20:39:45 +0100

    Implementing userCheck and password hashing

---
+++ site/src/jvmMain/kotlin/com/ps/blog_multiplatform/api/UserCheck.kt
@@ -0,0 +1,51 @@
+package com.ps.blog_multiplatform.api
+
+import com.ps.blog_multiplatform.data.MongoDb
+import com.ps.blog_multiplatform.models.User
+import com.ps.blog_multiplatform.models.UserWithoutPassword
+import com.varabyte.kobweb.api.Api
+import com.varabyte.kobweb.api.ApiContext
+import com.varabyte.kobweb.api.data.getValue
+import com.varabyte.kobweb.api.http.setBodyText
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
+
+@Api(routeOverride = "usercheck")
+suspend fun checkUser(context: ApiContext) {
+    try {
+        val userRequest =
+            context.req.body?.decodeToString()?.let { Json.decodeFromString<User>(it) }
+        val user = userRequest?.let {
+            context.data.getValue<MongoDb>().checkUserExistence(
+                User(username = it.username, password = hashPassword(it.password))
+            )
+        }
+        if (user != null) {
+            context.res.setBodyText(
+                text = Json.encodeToString(
+                    UserWithoutPassword(
+                        id = user.id, username = user.username
+                    )
+                )
+            )
+        } else {
+            context.res.setBodyText(text = Json.encodeToString(Exception("User doesn't exist.")))
+        }
+    } catch (e: Exception) {
+        context.res.setBodyText(text = Json.encodeToString(Exception(e.message)))
+    }
+}
+
+private fun hashPassword(password: String) : String {
+     val messageDigest = MessageDigest.getInstance("SHA-256")
+    val hashBytes = messageDigest.digest(password.toByteArray(StandardCharsets.UTF_8))
+    val hexString = StringBuffer()
+
+    for(byte in hashBytes){
+        hexString.append(String.format("%02x", byte))
+    }
+
+    return hexString.toString()
+}
---
+++ site/src/jvmMain/kotlin/com/ps/blog_multiplatform/models/UserWithoutPassword.kt
@@ -0,0 +1,12 @@
+package com.ps.blog_multiplatform.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import org.litote.kmongo.id.ObjectIdGenerator
+
+@Serializable
+data class UserWithoutPassword(
+    @SerialName(value = "_id")
+    val id: String = ObjectIdGenerator.newObjectId<String>().id.toHexString(),
+    val username: String = ""
+)
