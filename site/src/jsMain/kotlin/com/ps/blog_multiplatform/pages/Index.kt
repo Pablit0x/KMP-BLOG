678ee374794d9b1d13ff276ceec784124752ddf6
noner <pawel495@interia.pl>
2023-09-26 18:40:08 +0100

    Initial commit

---
+++ site/src/jsMain/kotlin/com/ps/blog_multiplatform/pages/Index.kt
@@ -0,0 +1,11 @@
+package com.ps.blog_multiplatform.pages
+
+import androidx.compose.runtime.*
+import com.varabyte.kobweb.core.Page
+
+@Page
+@Composable
+fun HomePage() {
+    // TODO: Add content here
+    println("Hello World!")
+}
